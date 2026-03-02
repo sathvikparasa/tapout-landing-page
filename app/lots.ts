3a6558947cc13dab575c50126a7a4f01b9f26711
Sathvik Parasa <saparasa@ucdavis.edu>
2026-03-02 13:48:46 -0800

    fix MU and Pavilion coordinates

--- app/lots.ts
+++ app/lots.ts
@@ -1,6 +1,6 @@
 export const lots = [
-  { name: 'Pavilion Structure',   code: 'HUTCH',   lat: 38.5391441, lng: -121.7600127 },
-  { name: 'Quad Structure',       code: 'MU',      lat: 38.5442216, lng: -121.7564567 },
+  { name: 'Pavilion Structure',   code: 'HUTCH',   lat: 38.539674579414715,  lng: -121.75836514704442 },
+  { name: 'Quad Structure',       code: 'MU',      lat: 38.54451981723509,   lng: -121.74950799295135 },
   { name: 'Lot 25',               code: 'ARC',     lat: 38.5433,    lng: -121.7574    },
   { name: 'Tercero Parking Lot',  code: 'TERCERO', lat: 38.534834,  lng: -121.756463  },
 ];
